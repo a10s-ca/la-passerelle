c838625ece20366357cdcaa1192882b93894983d
Christian Roy <roychristian@gmail.com>
2023-01-13 13:15:46 -0500

    Ajout de l'exemple de code pour le menu

--- doc/exemples.md
+++ doc/exemples.md
@@ -19,3 +19,34 @@ Dans ce cas, il est suggéré de créer une automatisation avec un déclencheur 
 Il peut arriver que la synchronisation d'une vue (ou d'une table) en entier ne fonctionne pas, parce que la taille des données fait en sorte que le script principal aurait besoin de plus de 30 secondes d'exécution, la limite imposée par Airtable.
 
 Dans ce cas, il est recommandé d'appeler le script principal pour chaque enregistrement, comme illustré dans [loopOverRecordsSync.js](../scripts/loopOverRecordsSync.js)
+
+## [Avancé] Un seul script pour plusieurs types d'opérations, et pour maintenir une seule table de correspondances
+
+Dans le cas où plusieurs tables doivent être synchronisées, avec des variantes pour un seul enregistrement à la fois, ou une vue entière, ou la table entière, il peut devenir compliqué de gérer les nombreuses extensions. Cette complexité affecte à la fois les développeurs qui configurent les extensions, que les utilisateurs qui doivent naviguer à travers les tables et les extensions.
+
+Une approche pour résoudre ce problème consiste à utiliser un seul script contenant la table de correspondance des champs de toutes les tables, qui sait réaliser tous les types de synchronisations, et qui peut détecter le type de synchronisation en fonction du contexte de l'utilisateur.
+
+Le script [le_menu.js](../scripts/le_menu.js), combiné à une table Airtable permettant de stocker des configurations, implante cette approche. Installez le dans une extension de type script, comme pour les autres scripts. Ensuite, indiquez l'URL du webhook déclencheur du script principal dans le champ de configuration prévu à cette fin.
+
+Pour fonctionner, le script s'appuie sur des configurations qui doivent être stockées dans une table dédiée à cette fin. La table, nommée «Menu La Passerelle», contient des enregistrements qui correspondent à des types de synchronisation (telle vue de telle table; ou telle table en entier; ou un seul enregistrement pour une table donnée; etc.). Elle doit contenir les champs suivants:
+
+|Champ|Priorité|Type et contenu|Utilité|
+|---|---|---|---|
+|Identificateur|Obligatoire|Au choix|Sera affichée à l'usager pour sélectionner l'opération qu'il souhaite réaliser|
+|Notes|Optionnel|Texte long|Pour documenter l'utilisation de ce type de synchronisation|
+|Type|Obligatoire|Texte ou selection simple|Indique le type de synchronisation. Les options possible sont «Toute la table», «Une vue» et «Un enregistrement» (l'orthographe doit être respecté strictement)|
+|Table|Obligatoire|Texte ou selection simple|Indique la table visée par la synchronisation. L'orthographe doit strictement être identique au nom de la table.|
+|Vue|Obligatoire|Texte ou selection simple|Indique la vue visée par la synchronisation, si le type concerne une vue|
+|Statut|Optionnel|Texte ou selection simple|Doit contenir le statut WordPress qui sera appliqué, par exemple `publish` ou `draft`|
+|Lancer la synchronisation (nom libre)|Bouton|Obligatoire|Bouton qui déclenche l'exécution du script du menu|
+
+Voici un exemple d'une telle table:
+
+![Exemple de table pour Le Menu](../images/le_menu.png)
+
+Il est également possible d'ajouter des champ de type bouton, qui déclenche le script à partir d'autres tables. Leur utilisation permet d'avoir des boutons de synchronisation individuelle d'un seul champ pour n'importe quelle table, tout en bénéficiant de la configuration unique des tables de correspondance dans le script Le Menu. À noter que dans cette configuration, le statut sera toujours `publish`, sauf si le nom de la vue d'où le script est appelé contient le mot «brouillon».
+
+Pour éviter la confusion :
+
+* un clic sur le bouton dans la table du Menu exécute l'opération configurée dans l'enregistrement concerné du menu ;
+* un clic sur le bouton dans une autre table synchronise un seul champ.
---
+++ scripts/le_menu.js
@@ -0,0 +1,152 @@
+// Configurations du Menu
+const config = input.config({
+    title: 'Le Menu de la Passerelle',
+    description: 'Un script pour les gouverner tous...',
+    items: [
+        input.config.text('webhookUrl', {
+            label: 'URL du webhook déclenchant le script principal',
+        })
+    ]
+});
+
+// Pour accéder au script principal de La Passerelle
+const webhookUrl = config.webhookUrl;
+
+// Configurations générales des synchronisations pour chaque table
+let baseParams = {
+    'Artistes': {
+        syncType: 'record',
+        airtable: {
+            table: 'Artistes',
+            // for record syncType, we should have recordId here; instead we will set it in the for loop below
+            wpIdField: 'Identifiant WordPress',
+            titleField: 'Identification',
+            wpUrlField: 'URL WordPress',
+            lastSyncFieldName: 'Derniere synchro'
+        },
+        wordpress: {
+            postType: 'artiste',
+            acf: {
+                    'nom': 'Nom',
+                    'prenom': 'Prénom',
+                    'courriel': 'Courriel',
+                    'telephone': 'Téléphone',
+                    'description': 'Description',
+                    'description_riche': 'Description riche',
+                    'region': 'Région',
+                    'date': 'Date',
+                    'date_et_heure': 'Date et heure',
+                    'photo': 'Photo',
+                    'fichier_pdf': 'Fichier PDF',
+                    'lookup': {
+                        'field': 'lookup',
+                        'model': 'taxo_information_liee'
+                    },
+                    'type-de-membre': {
+                        field: 'Type de membre',
+                        model: 'type-de-membre'
+                        },
+                    'oeuvres': {
+                        field: 'ID WP des oeuvres',
+                        model: 'oeuvre'
+                    },
+                    'cas_du_single_select': {
+                        field: 'Cas du single select',
+                        model: 'cas_du_single_select'
+                    }
+            },
+            'content': 'Contenu',
+            'featured_media': 'Photo'
+        }
+    },
+    'Oeuvres': {
+        syncType: 'record',
+        airtable: {
+            table: 'Oeuvres',
+            wpIdField: 'Identifiant WordPress',
+            titleField: 'Nom'
+        },
+        wordpress: {
+            postType: 'oeuvre',
+            acf: {
+                    'annee_de_creation': 'Année de création'
+            }
+        }
+    }
+}
+
+// Identifier la table du menu
+const menuTableName = 'Menu La Passerelle';
+let menuTable = base.getTable(menuTableName);
+
+// Déterminer la commande à exécuter en fonction du contexte de l'usager.
+let commande, syncTable, syncTableName, syncType, status, record;
+let cursorTableId = cursor.activeTableId;
+let cursorTable = base.getTable(cursorTableId);
+
+// Si l'usager est dans la table du menu, on lui présente toutes les commandes.
+if (cursorTable == menuTable) {
+    commande = await input.recordAsync('Quelle commande?', menuTable);
+    syncTableName = commande.getCellValueAsString('Table');
+    syncTable = base.getTable(syncTableName);
+    syncType = commande.getCellValueAsString('Type');
+    status = commande.getCellValueAsString('Statut')
+
+// Si l'usager est dans une autre table, on sait qu'il veut synchroniser un enregistrement
+} else {
+    syncType = "Enregistrement d'une autre table";
+    syncTable = cursorTable;
+    syncTableName = syncTable.name;
+    status = 'publish';
+    if (syncTable.getView(cursor.activeViewId).name.toLowerCase().includes("brouillon")) status = 'draft';
+    record = await input.recordAsync('Quel enregistrement', syncTable);
+}
+
+output.markdown("# Synchronisation de données...");
+output.markdown("Table: " + syncTableName);
+output.markdown("Statut: `" + status + "`");
+output.markdown("Type: " + syncType);
+
+// Identifier quels enregistrements seront synchronisés, selon les configurations du menu
+var query, records;
+switch(syncType) {
+    case "Toute la table":
+        query = await syncTable.selectRecordsAsync({});
+        records = query.records;
+        break;
+    case "Une vue":
+        let vue = commande.getCellValueAsString('Vue');
+        query = await syncTable.getView(vue).selectRecordsAsync({});
+        records = query.records;
+        break;
+    case "Un enregistrement":
+        record = await input.recordAsync("Pour quel enregistrement?", syncTable);
+        records = [record];
+        break;
+    case "Enregistrement d'une autre table":
+        records = [record];
+        break;
+}
+
+// Obtenir les paramètres de la table ciblée, et les personnaliser avec le statut choisi
+let params = baseParams[syncTableName];
+params.wordpress.status = status;
+
+// Itérer sur tous les enregistrements et les synchroniser
+for (let record of records) {
+    output.text(record.name + " " + record.id);
+    params.airtable.recordId = record.id;
+    let config = {};
+    config.params = JSON.stringify(params);
+      // we stringify the params key, which will be double-stringified in the request body, so that we can
+      // pass it serialized to the automation script, which will deserialize it. This is because Airtable will not
+      // allow us to use specific keys in automation script config.
+    let response = await remoteFetchAsync(webhookUrl, {
+        method: 'POST',
+        body: JSON.stringify(config),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    console.log(await response.text());
+}
